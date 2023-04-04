a0b541292eacd5f04d657b0bb21ed888c48ed234
Lucas Bendix Jolibois <lucasjolibois@icloud.com>
2023-04-05 01:31:43 +0200

    Update create.php

--- create.php
+++ create.php
@@ -63,62 +63,59 @@ if ( $_SERVER['REQUEST_METHOD'] == 'POST') {
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
 </head>
-<body>
-    <div class="max-w-6xl mx-auto">
-        <h1>New Product</h1>
+<body class="bg-gray-100">
+  <div class="container mx-auto px-4 py-8">
+    <h1 class="text-3xl font-bold text-center mb-8">Create a New Product</h1>
+    
+    <?php
+    if (!empty($errorMessage)){
+        echo "<p class='text-red-500 mb-4'>$errorMessage</p>";
+    }
+    ?>
 
-        <?php
-        if (!empty($errorMessage)){
-            echo "
-            <p>$errorMessage</p>";
-        }
-        ?>
+    <form class="max-w-lg mx-auto" method="post">
+      <div class="mb-4">
+        <label class="block font-bold mb-2 text-gray-700" for="title">Title</label>
+        <input id="title" name="title" value="<?php echo $title; ?>" type="text" placeholder="Enter the title" class="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300">
+      </div>
+      
+      <div class="mb-4">
+        <label class="block font-bold mb-2 text-gray-700" for="brand">Brand</label>
+        <input id="brand" name="brand" value="<?php echo $brand; ?>" type="text" placeholder="Enter the brand" class="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300">
+      </div>
+      
+      <div class="mb-4">
+        <label class="block font-bold mb-2 text-gray-700" for="descr">Description</label>
+        <textarea id="descr" name="descr" placeholder="Enter the description" class="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300"><?php echo $descr; ?></textarea>
+      </div>
+      
+      <div class="mb-4">
+        <label class="block font-bold mb-2 text-gray-700" for="price">Price</label>
+        <input id="price" name="price" value="<?php echo $price; ?>" type="text" placeholder="Enter the price" class="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300">
+      </div>
+      
+      <div class="mb-4">
+        <label class="block font-bold mb-2 text-gray-700" for="main_image">Main Image URL</label>
+        <input id="main_image" name="main_image" value="<?php echo $main_image; ?>" type="text" placeholder="Enter the URL of the main image" class="w-full px-4 py-2 border rounded-lg shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300">
+      </div>
+      
+      <?php 
+      if ( !empty($successMessage)){
+          echo "<p class='text-green-500 mb-4'>$successMessage</p>";
+      }
+      ?>
 
-        <form  method="post">
-        <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="title">
-            title
-            </label>
-            <input id="title" name="title" value="<?php echo $title; ?>" type="text" placeholder="title" class="shadow appearance-none border rounded w-6/9 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-        </div>
-        <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="brand">
-            brand
-            </label>
-            <input id="brand" name="brand" value="<?php echo $brand; ?>" type="text" placeholder="brand" class="shadow appearance-none border rounded w-6/9 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-        </div>
-        <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="descr">
-            descr
-            </label>
-            <input id="descr" name="descr" value="<?php echo $descr; ?>" type="text" placeholder="descr" class="shadow appearance-none border rounded w-6/9 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-        </div>
-        <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="price">
-            price
-            </label>
-            <input id="price" name="price" value="<?php echo $price; ?>" type="text" placeholder="price" class="shadow appearance-none border rounded w-6/9 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-        </div>
-        <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="main_image">
-            main_image
-            </label>
-            <input id="main_image" name="main_image" value="<?php echo $main_image; ?>" type="text" placeholder="main_image" class="shadow appearance-none border rounded w-6/9 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-        </div>
-
-        <?php 
-        if ( !empty($successMessage)){
-            echo "
-            <p>$successMessage</p>
-            ";
-        }
-        ?>
-
-        <div>
-            <button type="submit" class="bg-blue-500 py-2 px-3">submit</button>
-            <button class="bg-blue-500 py-2 px-3" href="/php-webshop/admin.php" role="button">cancel</button>
-        </div>
-        </form>
-    </div>
+      <div class="flex justify-end">
+        <button type="submit" class="bg-blue-500 py-2 px-4 text-white rounded-lg font-bold">
+          Create Product
+        </button>
+        <a href="/php-webshop/admin.php" class="bg-gray-400 py-2 px-4 text-white rounded-lg font-bold ml-4">
+          Cancel
+        </a>
+      </div>
+    </form>
+  </div>
 </body>
+
+
 </html>
