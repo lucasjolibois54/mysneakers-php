266492cc1e7dd89269ae791dae91673bda6db00b
Lucas Bendix Jolibois <lucasjolibois@icloud.com>
2023-04-05 01:31:25 +0200

    Update signup.php

--- signup.php
+++ signup.php
@@ -27,17 +27,42 @@ if (isset($_POST["signup"])) {
 <html>
 <head>
     <title>Sign Up</title>
+    <link href="https://unpkg.com/tailwindcss@^2.2/dist/tailwind.min.css" rel="stylesheet">
 </head>
-<body>
-    <?php if (isset($error)) { echo "<p>$error</p>"; } ?>
-    <form method="post">
-        <input type="text" name="name" placeholder="Name" required>
-        <input type="email" name="email" placeholder="Email" required>
-        <input type="address" name="address" placeholder="Address" required>
-        <input type="phone_number" name="phone_number" placeholder="Phone Number" required>
-        <input type="password" name="password" placeholder="Password" required>
-        <input type="password" name="confirm_password" placeholder="Confirm Password" required>
-        <button type="submit" name="signup">Sign Up</button>
-    </form>
+<body class="bg-gray-100 mt-36">
+    <div class="max-w-md mx-auto my-12 bg-white p-8 border border-gray-300 shadow-md">
+        <h1 class="text-xl font-medium mb-6 text-center">Sign Up</h1>
+        <?php if (isset($error)) { echo "<p class='text-red-500 mb-4'>$error</p>"; } ?>
+        <form method="post">
+            <div class="mb-4">
+                <label class="block text-gray-700 font-medium mb-2" for="name">Name</label>
+                <input class="border border-gray-300 p-2 w-full rounded-md" type="text" name="name" id="name" placeholder="Name" required pattern="[A-Za-z]+">
+            </div>
+            <div class="mb-4">
+                <label class="block text-gray-700 font-medium mb-2" for="email">Email</label>
+                <input class="border border-gray-300 p-2 w-full rounded-md" type="email" name="email" id="email" placeholder="Email" required>
+            </div>
+            <div class="mb-4">
+                <label class="block text-gray-700 font-medium mb-2" for="address">Address</label>
+                <input class="border border-gray-300 p-2 w-full rounded-md" type="address" name="address" id="address" placeholder="Address" required pattern="[A-Za-z0-9 ,]+">
+            </div>
+            <div class="mb-4">
+                <label class="block text-gray-700 font-medium mb-2" for="phone_number">Phone Number</label>
+                <input class="border border-gray-300 p-2 w-full rounded-md" type="tel" name="phone_number" id="phone_number" placeholder="Phone Number" required pattern="[0-9]{8}">
+            </div>
+            <div class="mb-4">
+                <label class="block text-gray-700 font-medium mb-2" for="password">Password</label>
+                <input class="border border-gray-300 p-2 w-full rounded-md" type="password" name="password" id="password" placeholder="Password" required>
+            </div>
+            <div class="mb-6">
+                <label class="block text-gray-700 font-medium mb-2" for="confirm_password">Confirm Password</label>
+                <input class="border border-gray-300 p-2 w-full rounded-md" type="password" name="confirm_password" id="confirm_password" placeholder="Confirm Password" required>
+            </div>
+            <div class="flex items-center justify-between">
+                <button class="border-2 rounded-full px-3 py-2 border border-black text-lg" type="submit" name="signup">Sign Up</button>
+                <a class="text-blue-500 hover:text-blue-600 font-medium" href="login.php">Already have an account?</a>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
