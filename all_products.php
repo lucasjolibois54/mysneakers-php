6950a64a02d39b666711db271bb7055a13174a57
Lucas Bendix Jolibois <lucasjolibois@icloud.com>
2023-04-05 01:31:48 +0200

    Update all_products.php

--- all_products.php
+++ all_products.php
@@ -11,37 +11,6 @@
     <link href="styles.css" rel="stylesheet">
 </head>
 <body>
-<!-- <nav>
-  <ul class="menu">
-    <li><a href="#">Home</a></li>
-    <li><a href="#">Products</a>
-      <ul class="sub-menu">
-        <li><a href="#">Men</a>
-          <ul class="sub-sub-menu">
-            <li><a href="#">Shoes</a></li>
-            <li><a href="#">Clothing</a></li>
-            <li><a href="#">Accessories</a></li>
-          </ul>
-        </li>
-        <li><a href="#">Women</a>
-          <ul class="sub-sub-menu">
-            <li><a href="#">Shoes</a></li>
-            <li><a href="#">Clothing</a></li>
-            <li><a href="#">Accessories</a></li>
-          </ul>
-        </li>
-        <li><a href="#">Kids</a>
-          <ul class="sub-sub-menu">
-            <li><a href="#">Shoes</a></li>
-            <li><a href="#">Clothing</a></li>
-            <li><a href="#">Accessories</a></li>
-          </ul>
-        </li>
-      </ul>
-    </li>
-    <li><a href="#">About Us</a></li>
-  </ul>
-</nav> -->
 
 <header>
         <nav class="flex flex-wrap items-center justify-between w-full  py-8 text-lg text-gray-700 px-20">
@@ -75,6 +44,7 @@
       </li>
     <li><a href="login.php#">Login</a></li>
     <li><a href="logout.php#">Logout</a></li>
+    <li><a href="cart.php#">Cart</a></li>
   </ul>
             </div>
         </nav>
@@ -120,6 +90,12 @@
                    
                 <h3 className='mt-4 text-sm text-gray-700'>$row[title]</h3>
                 <p className='mt-1 text-lg font-medium text-gray-900'>$row[price] DKK.</p>
+                <form method='POST' action='cart.php'>
+                <input type='hidden' name='product_id' value='$row[id]'>
+                <input type='hidden' name='product_title' value='$row[title]'>
+                <input type='hidden' name='product_price' value='$row[price]'>
+                <button type='submit' class='border-2 rounded-md px-3 py-2 border border-black text-lg mt-3'>Add to Cart</button>
+                </form>
                 </div> 
                     ";
                 }
@@ -127,6 +103,40 @@
 </div>
             
     </div>
+    
+
+    <footer class="bg-black text-white py-8 mt-36">
+  <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="flex flex-wrap justify-between">
+      <div class="w-full lg:w-1/3 mb-8 lg:mb-0">
+        <h2 class="text-lg font-bold mb-4">About Us</h2>
+        <p class="mb-4 pr-8">We are a leading online sneaker store, offering the latest sneakers from top brands at competitive prices.</p>
+        <p><a href="#" class="text-blue-500 hover:text-blue-300">Learn More &rarr;</a></p>
+      </div>
+      <div class="w-full lg:w-1/3 mb-8 lg:mb-0">
+        <h2 class="text-lg font-bold mb-4">Contact Us</h2>
+        <p class="mb-4">123 Main Street<br>Anytown, USA 12345<br>Phone: (123) 456-7890</p>
+        <p><a href="#" class="text-blue-500 hover:text-blue-300">Send Us a Message &rarr;</a></p>
+      </div>
+      <div class="w-full lg:w-1/3 mb-8 lg:mb-0">
+        <h2 class="text-lg font-bold mb-4">Follow Us</h2>
+        <p class="mb-4">Stay up to date with our latest news and promotions.</p>
+        <div class="flex">
+          <a href="index.php" class="text-white hover:text-blue-300 mr-4">Home</i></a>
+          <a href="all_products.php" class="text-white hover:text-blue-300 mr-4">All Products</a>
+          <a href="login.php" class="text-white hover:text-blue-300 mr-4">Login</a>
+          <a href="admin.php" class="text-white hover:text-blue-300">Admin</a>
+        </div>
+      </div>
+    </div>
+    <div class="border-t border-gray-700 mt-8 pt-8 text-sm text-center">
+      <p>&copy; 2023 MySneakers. All rights reserved.</p>
+    </div>
+  </div>
+</footer>
+
+
+
     <script src="main.js"></script>
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
             <script>
