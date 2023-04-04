63b6d1a0ac7e04390295d6aacb460799343c0849
Lucas Bendix Jolibois <lucasjolibois@icloud.com>
2023-04-05 01:31:30 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,21 +1,26 @@
 <?php
 session_start();
 
+$_SESSION['loggedin'] = true;
+
+setcookie('loggedin', true, time() + (86400 * 30), '/');
+
+
 if (isset($_SESSION["user_id"])) {
-    // User is already logged in, redirect to index page
+
     header("Location: index.php");
     exit;
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Handle login form submission
+
     $email = $_POST["email"];
     $password = $_POST["password"];
 
-    // Sanitize the user input
+
     $email = filter_var($email, FILTER_SANITIZE_EMAIL);
 
-    // Connect to database and check if email and password match a user in the database
+
     $conn = mysqli_connect("localhost", "root", "", "php_webshop_database");
     $stmt = mysqli_prepare($conn, "SELECT * FROM users WHERE email = ?");
     mysqli_stmt_bind_param($stmt, "s", $email);
@@ -23,10 +28,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $result = mysqli_stmt_get_result($stmt);
     $user = mysqli_fetch_assoc($result);
     if ($user && password_verify($password, $user["password"])) {
-        // Set session variables
+
         $_SESSION["user_id"] = $user["id"];
         
-        // Check if user is admin and redirect to appropriate page
+
         if ($user["email"] == "admin@admin.com") {
             header("Location: admin.php");
             exit;
@@ -35,7 +40,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             exit;
         }
     } else {
-        // Invalid email or password
         $error = "Invalid email or password.";
     }
 }
@@ -46,15 +50,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html>
 <head>
     <title>Login</title>
+    <link href="https://unpkg.com/tailwindcss@^2.2/dist/tailwind.min.css" rel="stylesheet">
 </head>
-<body>
-    <?php if (isset($error)) { echo "<p>$error</p>"; } ?>
-    <form method="post">
-        <input type="email" name="email" placeholder="Email" required>
-        <input type="password" name="password" placeholder="Password" required>
-        <button type="submit">Login</button>
-    </form>
-    <br>
-    <a href="signup.php">Sign up</a>
+<body class="bg-gray-100 mt-36">
+    <div class="max-w-md mx-auto my-16 bg-white p-8 rounded-md shadow-md">
+        <h1 class="text-2xl text-center mb-4 font-bold">Login</h1>
+        <?php if (isset($error)) { echo "<p class='text-red-500 mb-4'>$error</p>"; } ?>
+        <form method="post" class="space-y-4">
+            <div class="flex flex-col">
+                <label for="email" class="text-gray-700 font-semibold mb-2">Email</label>
+                <input type="email" name="email" id="email" placeholder="Email" required class="border-2 border-gray-300 p-2 rounded-md">
+            </div>
+            <div class="flex flex-col">
+                <label for="password" class="text-gray-700 font-semibold mb-2">Password</label>
+                <input type="password" name="password" id="password" placeholder="Password" required class="border-2 border-gray-300 p-2 rounded-md">
+            </div>
+            <button type="submit" class="border-2 rounded-full px-3 py-2 border border-black text-lg">Login</button>
+        </form>
+        <div class="text-center mt-4 text-gray-700">
+            <p>Don't have an account? <a href="signup.php" class="text-blue-500 font-semibold">Sign up</a></p>
+        </div>
+    </div>
 </body>
 </html>
