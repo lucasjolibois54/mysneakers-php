6f1833c29f8353b464c911fbacf61d612a362009
Lucas Bendix Jolibois <lucasjolibois@icloud.com>
2023-04-05 01:31:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -45,12 +45,13 @@
       </li>
     <li><a href="login.php#">Login</a></li>
     <li><a href="logout.php#">Logout</a></li>
+    <li><a href="cart.php#">Cart</a></li>
   </ul>
             </div>
         </nav>
       </header>
-
-    <section>
+      <h1 class="text-3xl ml-8 mt-10 mb-10 md:hidden font-bold mb-5">Discover limited <br/>sneakers without <br/>limitation</h1>
+    <section class="hidden md:block">
     <div class="grid grid-cols-2 h-screen">
   <!-- Left column -->
   <div class="flex flex-col">
@@ -64,14 +65,15 @@
   <p class="ml-28 font-medium text-md mt-6 flex space-x-6"><span>SHOP NOW</span><span>◯</span><span>SHOP NOW</span><span>◯</span><span>SHOP NOW</span></p>
   </div></div>
   <!-- Right column -->
-  <div class="h-screen">
+  <div class="md:block hidden h-screen">
     <img src="hero.webp" alt=" image" class="h-full w-full object-cover object-center">
   </div>
 </div>
 
     </section>
 
-    <div class="bg-gray-100">
+    <h1 class="hidden font-bold mb-5">// Featured Products</h1>
+    <div class="bg-gray-100 hidden md:block">
       
     <div class="grid grid-cols-5 gap-4 max-w-7xl ml-28 pl-8" style="padding-top:170px">
   <div class="col-span-4"> 
@@ -83,10 +85,10 @@
   </div>
 </div></div>
 
-    <div class=" max-w-7xl ml-28 pl-8">
+    <div class=" max-w-7xl md:ml-28 pl-8 mr-8 mb-20">
     <!-- <div class=" mx-28 px-8"> -->
         <br/>
-            <div class='grid grid-cols-3 gap-10'>
+            <div class='grid grid-cols-1 md:grid-cols-3 gap-10'>
                 <?php
                 
                 session_start();
@@ -118,6 +120,12 @@
                    
                 <h3 className='mt-4 text-sm text-gray-700'>$row[title]</h3>
                 <p className='mt-1 text-lg font-medium text-gray-900'>$row[price] DKK.</p>
+                <form method='POST' action='cart.php'>
+                <input type='hidden' name='product_id' value='$row[id]'>
+                <input type='hidden' name='product_title' value='$row[title]'>
+                <input type='hidden' name='product_price' value='$row[price]'>
+                <button type='submit' class='border-2 rounded-md px-3 py-2 border border-black text-lg mt-3'>Add to Cart</button>
+                </form>
                 </div> 
                     ";
                 }
@@ -129,6 +137,37 @@
     <!-- <section>
       <img class="w-screen mt-56 mb-48" src="cover-img-home.png"/>
     </section> -->
+
+
+    <footer class="bg-black text-white py-8 mt-36">
+  <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="flex flex-wrap justify-between">
+      <div class="w-full lg:w-1/3 mb-8 lg:mb-0">
+        <h2 class="text-lg font-bold mb-4">About Us</h2>
+        <p class="mb-4 pr-8">We are a leading online sneaker store, offering the latest sneakers from top brands at competitive prices.</p>
+        <p><a href="#" class="text-blue-500 hover:text-blue-300">Learn More &rarr;</a></p>
+      </div>
+      <div class="w-full lg:w-1/3 mb-8 lg:mb-0">
+        <h2 class="text-lg font-bold mb-4">Contact Us</h2>
+        <p class="mb-4">123 Main Street<br>Anytown, USA 12345<br>Phone: (123) 456-7890</p>
+        <p><a href="#" class="text-blue-500 hover:text-blue-300">Send Us a Message &rarr;</a></p>
+      </div>
+      <div class="w-full lg:w-1/3 mb-8 lg:mb-0">
+        <h2 class="text-lg font-bold mb-4">Follow Us</h2>
+        <p class="mb-4">Stay up to date with our latest news and promotions.</p>
+        <div class="flex">
+          <a href="index.php" class="text-white hover:text-blue-300 mr-4">Home</i></a>
+          <a href="all_products.php" class="text-white hover:text-blue-300 mr-4">All Products</a>
+          <a href="login.php" class="text-white hover:text-blue-300 mr-4">Login</a>
+          <a href="admin.php" class="text-white hover:text-blue-300">Admin</a>
+        </div>
+      </div>
+    </div>
+    <div class="border-t border-gray-700 mt-8 pt-8 text-sm text-center">
+      <p>&copy; 2023 MySneakers. All rights reserved.</p>
+    </div>
+  </div>
+</footer>
 
     <script src="main.js"></script>
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
