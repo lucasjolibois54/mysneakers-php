928bd638908e741b575406c02298edd5fcd0d720
Lucas Bendix Jolibois <lucasjolibois@icloud.com>
2023-04-05 02:22:15 +0200

    Update admin_login.php

--- admin_login.php
+++ admin_login.php
@@ -1,76 +1,56 @@
 <?php
-// Start session
 session_start();
 
-// Check if user is already logged in
-if (isset($_SESSION['email'])) {
-  header("Location: admin.php");
-  exit();
+if (isset($_SESSION["user_id"])) {
+    header("Location: admin.php");
+    exit;
 }
 
-// Check if user submitted the form
-if (isset($_POST['login'])) {
-  // Connect to database
-  $conn = mysqli_connect("localhost", "username", "password", "php_webshop_database");
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST["email"];
+    $password = $_POST["password"];
 
-  // Check connection
-  if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-  }
-
-  // Get user input
-  $email = $_POST['email'];
-  $password = $_POST['password'];
-
-  // Query database to check if user exists
-  $sql = "SELECT * FROM users WHERE email='$email' AND password='$password'";
-  $result = mysqli_query($conn, $sql);
-
-  // Check if query returned any result
-  if (mysqli_num_rows($result) > 0) {
-    // User exists, set session variables and cookies
-    $row = mysqli_fetch_assoc($result);
-    $_SESSION['email'] = $row['email'];
-    $_SESSION['user_id'] = $row['user_id'];
-    setcookie("user_id", $row['user_id'], time() + (86400 * 30), "/");
-    setcookie("email", $row['email'], time() + (86400 * 30), "/");
-    
-    // Check if user is an admin
-    if ($row['email'] == "admin@admin.com") {
-      header("Location: admin.php");
+    $conn = mysqli_connect("localhost", "root", "", "php_webshop_database");
+    $stmt = mysqli_prepare($conn, "SELECT * FROM users WHERE email = ?");
+    mysqli_stmt_bind_param($stmt, "s", $email);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    $user = mysqli_fetch_assoc($result);
+    if ($user && password_verify($password, $user["password"])) {
+       // $_SESSION["user_id"] = $user["id"];
+        if ($user["email"] == "admin@admin.com") {
+            header("Location: admin.php");
+            exit;
+        } else {
+            $error = "You are not an admin!";
+        }
     } else {
-      header("Location: admin.php");
+        $error = "Invalid email or password.";
     }
-    exit();
-  } else {
-    // Invalid login credentials
-    $error = "Invalid login credentials.";
-  }
-
-  // Close database connection
-  mysqli_close($conn);
 }
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Login</title>
+    <title>Login</title>
+    <link href="https://unpkg.com/tailwindcss@^2.2/dist/tailwind.min.css" rel="stylesheet">
 </head>
-<body>
-  <h1>Login</h1>
-  <?php
-  // Display error message if there was an error
-  if (isset($error)) {
-    echo "<p>$error</p>";
-  }
-  ?>
-  <form method="post">
-    <label>Email:</label>
-    <input type="email" name="email" required><br>
-    <label>Password:</label>
-    <input type="password" name="password" required><br>
-    <input type="submit" name="login" value="Login">
-  </form>
+<body class="bg-gray-100 mt-36">
+    <div class="max-w-md mx-auto my-16 bg-white p-8 rounded-md shadow-md">
+        <h1 class="text-2xl text-center mb-4 font-bold">Admin Login</h1>
+        <?php if (isset($error)) { echo "<p class='text-red-500 mb-4'>$error</p>"; } ?>
+        <form method="post" class="space-y-4">
+            <div class="flex flex-col">
+                <label for="email" class="text-gray-700 font-semibold mb-2">Email</label>
+                <input type="email" name="email" id="email" placeholder="Email" required class="border-2 border-gray-300 p-2 rounded-md">
+            </div>
+            <div class="flex flex-col">
+                <label for="password" class="text-gray-700 font-semibold mb-2">Password</label>
+                <input type="password" name="password" id="password" placeholder="Password" required class="border-2 border-gray-300 p-2 rounded-md">
+            </div>
+            <button type="submit" class="border-2 rounded-full px-3 py-2 border border-black text-lg">Login</button>
+        </form>
+    </div>
 </body>
 </html>
